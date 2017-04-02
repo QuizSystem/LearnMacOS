8bf0351b62937bdf87471d4db63f43be99e214a3
Nguyen Van Thieu <nguyen.van.thieub>
2017-04-02 14:25:43 +0700

    Init Project

---
+++ LearnMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LearnMacOS
+//
+//  Created by Thieu Mao on 4/2/17.
+//  Copyright © 2017 Hay Nhanh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LearnMacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LearnMacOS
+//
+//  Created by Thieu Mao on 4/2/17.
+//  Copyright © 2017 Hay Nhanh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
